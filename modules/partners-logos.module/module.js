8a39eb280b34a5547852dfd0a097881ba524ea3b
Ramiro Iriñiz <mrlomein@gmail.com>
2022-08-04 10:30:36 -0300

    Ramiro First Commit

---
+++ modules/partners-logos.module/module.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($) { 
+    $(".partner-inner").slick({
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        infinite: true,
+        arrows: false,
+        responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                centerMode: true,
+                centerPadding: 50,
+            }
+        }
+        ]
+    });
+    $(window).resize(function(){
+        $('.partner-inner')[0].slick.refresh();
+    });
+});
