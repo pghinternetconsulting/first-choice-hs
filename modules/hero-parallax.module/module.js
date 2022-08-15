923b0263e4469a5beda98f4cd3e11abaa975f62c
Ramiro Iriñiz <mrlomein@gmail.com>
2022-08-15 10:09:39 -0300

    Hero Animated Text

--- modules/hero-parallax.module/module.js
+++ modules/hero-parallax.module/module.js
@@ -4,23 +4,42 @@ jQuery(document).ready(function($){
 
     //Actually define the slideShow()
     function slideShow(){
+        split = function (element) {
+            words = $(element).text().split('');
+            for (i in words) {
+              words[i] = '<span>' + words[i] + '</span>';
+            }
+            text = words.join('');
+            $(element).html(text);
+        };
+          
+        textify = function(element,method,delay) {
+            split(element);
+            $(element + ' span').css('opacity','0')
+            $(element + ' span').css('position','relative');
+            in_speed = 10;
+            count = 0;
+            setTimeout(function(){
+              count = 0;
+              $(element + ' span').each(function () {
+                if(method == 'fade'){
+                  $(this).delay(0 + in_speed * count).animate({ opacity: '1' }, 200);
+                } else if(method == 'bounce'){
+                  $(this).delay(0 + in_speed * count).animate({ opacity: '1','top':'-4px'}, 220,'easeOutCubic');
+                  $(this).delay(0 + in_speed * count/4).animate({ opacity: '1','top':'0px'}, 220);
+                }
+                count++;
+              });
+            },delay);
+        };
     
         //*** Conditional & Variables ***//
-        
-        //Define the current img
         var current = $('.hero-subheading .show');
-        //If index != 0/false then show next img
-        var next = current.next().length ? 
-        current.next() :
-        // if index == false then show first img
-        current.siblings().first();
-
-        //*** Swap out the imgs and class ***//
+        var next = current.next().length ? current.next() : current.siblings().first();
         current.hide().removeClass('show');
-        next.fadeIn("slow").addClass('show');
-        
-        //*** Repeat function every 3 seconds ***//
+        next.addClass('show');
+        textify('.show','fade', 500);
         setTimeout(slideShow, 5000);
-    
     };
-}); //end ready
+
+}); 
