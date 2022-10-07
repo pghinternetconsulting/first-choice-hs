fcfcbe9af13c08889393515201becccfa4035705
Ramiro Iriñiz <mrlomein@gmail.com>
2022-10-07 11:49:16 -0300

    Logo Slider Module

---
+++ modules/logos-slider.module/module.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".slick-logo").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    infinite: true,
+    arrows: false
+  });
+});
