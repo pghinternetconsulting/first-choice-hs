3dee7f4b5a270b7fd39be142ee17ab9abb8e84bb
Ramiro Iriñiz <mrlomein@gmail.com>
2022-10-10 11:31:37 -0300

    Logos slider

--- modules/logos-slider.module/module.js
+++ modules/logos-slider.module/module.js
@@ -1,24 +1,43 @@
 jQuery(document).ready(function($) { 
   $(".slick-logo").slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 6,
+    slidesToScroll: 6,
     autoplay: true,
     autoplaySpeed: 3000,
-    dots: true,
+    dots: false,
     infinite: true,
     arrows: false,
     responsive: [
       {
-          breakpoint: 767,
+          breakpoint: 1300,
           settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
+              slidesToShow: 4,
+              slidesToScroll: 4,
               infinite: true,
-              dots: true,
-              centerMode: true,
+              dots: false,
               centerPadding: 50,
           }
-      }
+      },
+      {
+        breakpoint: 1150,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false,
+            centerPadding: 50,
+        }
+    },
+      {
+        breakpoint: 767,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: false,
+            centerPadding: 50,
+        }
+    }
       ]
   });
   $(window).resize(function(){
