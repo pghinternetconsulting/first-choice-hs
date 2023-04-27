7b99e4379df3831c8e10e1ca8a18a6e6ca9432a8
marianaforteza <mariana@piconsulting.com>
2023-04-27 18:56:15 -0300

    trainings

---
+++ modules/training-gallery.module/module.js
@@ -0,0 +1,9 @@
+var training__thumbs = document.querySelectorAll('.training__thumbs')
+var training__image  = document.querySelector('.training__image')
+
+
+training__thumbs.forEach(thumb => {
+    thumb.addEventListener('click', (e) => {
+        training__image.src = e.target.dataset.src
+    })
+})
