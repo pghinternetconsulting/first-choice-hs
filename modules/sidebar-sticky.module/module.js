2953d5855825f557cbef99f06a40689981929b20
marianaforteza <mariana@piconsulting.com>
2023-05-02 15:07:49 -0300

    #1 page

--- modules/sidebar-sticky.module/module.js
+++ modules/sidebar-sticky.module/module.js
@@ -2,8 +2,13 @@ var sticky_sidebars = document.querySelectorAll('.sidebar-sticky')
 
 sticky_sidebars.forEach(function(sidebar) {
     var sticky_column = sidebar.closest('.dnd-column');
+    var sticky_section = sidebar.closest('.dnd-section');
 
     if (sticky_column) {
         sticky_column.classList.add('sidebar-sticky__column');
+    }
+
+    if (sticky_section) {
+        sticky_section.classList.add('sidebar-sticky__section');
     }
 });
