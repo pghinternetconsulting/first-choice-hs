c315391f20913df0227539e066061c41ecaa8e6c
marianaforteza <mariana@piconsulting.com>
2023-05-08 17:03:02 -0300

    small fix in #1 Page

--- modules/sidebar-sticky.module/module.js
+++ modules/sidebar-sticky.module/module.js
@@ -12,3 +12,51 @@ sticky_sidebars.forEach(function(sidebar) {
         sticky_section.classList.add('sidebar-sticky__section');
     }
 });
+
+
+// Activate menu items when scrolling
+
+const makeSidebarActive = () => {
+    var sidebar_elements = document.querySelectorAll('.sidebar-sticky .hs-menu-item a')
+    var windowHeight = window.innerHeight;
+    var windowWidth = window.innerWidth;
+    var windowHeight = (windowHeight > 600) ? 600 : windowHeight;
+    console.log(windowHeight)
+
+    sidebar_elements.forEach((link) => {
+        var target_id = link.hash.replace('#', '')
+        var target = document.getElementById(target_id)
+    
+        if (target) {
+            var rect = target.getBoundingClientRect()
+    
+            if (rect.bottom > 0 && rect.top < windowHeight && rect.right > 0 && rect.left < windowWidth) {
+                // Element is visible
+                link.classList.add('active')
+              } else {
+                // Element is not visible
+                link.classList.remove('active')
+              }
+        }
+    })
+}
+
+document.addEventListener('scroll', makeSidebarActive)
+
+// Move anchors to columns so active sidebar works better
+const moveAnchorstoColumns = () => {
+    var sidebar_elements = document.querySelectorAll('.sidebar-sticky .hs-menu-item a')
+
+    sidebar_elements.forEach((link) => {
+        var target_id = link.hash.replace('#', '')
+        var target = document.getElementById(target_id)
+    
+        if (target) {
+            var column = target.closest('.dnd-column')
+            console.log(column)
+            column.setAttribute('id', target_id)
+        }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', moveAnchorstoColumns)
